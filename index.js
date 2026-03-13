ac5a5cd6ec3b58ccd68740533b114c773a69cdc1
Alexander_Tirsky <tirsky06@gmail.com>
2026-03-13 20:46:46 +1000

    added controls camera and event click change color blue

--- index.js
+++ index.js
@@ -1,28 +1,16 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 //сцена
 const scene = new THREE.Scene();
 
-// // Свет всю сцену освещает без теней
-// const ambientLight = new THREE.AmbientLight("white", 0.5);
-// scene.add(ambientLight);
+// Свет всю сцену освещает без теней
+const ambientLight = new THREE.AmbientLight("white", 0.5);
+scene.add(ambientLight);
 
-// // свет типо солнца есть тени у объектов
-// const dirLight = new THREE.DirectionalLight("white", 1);
-// dirLight.position.set(5, 5, 5);
-// scene.add(dirLight);
-
-// свет поинт лайт типо лампочка
-// const pointLight = new THREE.PointLight("white", 10, 100);
-// pointLight.position.set(0.5, 1, 1);
-// scene.add(pointLight);
-
-// const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.5);
-// scene.add(pointLightHelper);
-
-// свет спот лайт типо фанарик направленый луч
-const spotLight = new THREE.SpotLight("white", 1);
-spotLight.position.set(1, 1, 1);
-scene.add(spotLight);
+// свет типо солнца есть тени у объектов
+const dirLight = new THREE.DirectionalLight("white", 1);
+dirLight.position.set(5, 5, 5);
+scene.add(dirLight);
 
 //камера
 const camera = new THREE.PerspectiveCamera(
@@ -39,21 +27,56 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
+//управление камерой
+
+const contrls = new OrbitControls(camera, renderer.domElement);
+contrls.enableDamping = true;
+contrls.dampingFactor = 0.05;
+contrls.screenSpacePanning = false;
+contrls.minDistance = 2;
+contrls.maxDistance = 10;
+
 // создание фигру разных
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshStandardMaterial({ color: "red" });
 const cube = new THREE.Mesh(geometry, material);
 cube.position.set(0, 0, 0);
-
 scene.add(cube);
+
+const sphera = new THREE.Mesh(
+  new THREE.SphereGeometry(),
+  new THREE.MeshStandardMaterial({ color: "green" }),
+);
+sphera.position.x = 2;
+scene.add(sphera);
+
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2();
+
+function onMouseClick(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  raycaster.setFromCamera(mouse, camera);
+
+  const intersects = raycaster.intersectObjects(scene.children);
+
+  if (intersects.length > 0) {
+    intersects[0].object.material.color.set("blue");
+    alert("Clicked on item");
+  }
+}
+
+window.addEventListener("click", onMouseClick);
 
 // функция для постоянного рендеринга анимации
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  //   cube.rotation.x += 0.1;
+  //   cube.rotation.y += 0.1;
 
+  contrls.update();
   renderer.render(scene, camera);
 }
 animate();
