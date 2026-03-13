1da33ef221934dfe788c3b972638cb2a2a7cfe96
Alexander_Tirsky <tirsky06@gmail.com>
2026-03-13 19:32:43 +1000

    added light in scena

--- index.js
+++ index.js
@@ -1,7 +1,30 @@
 import * as THREE from "three";
-
+//сцена
 const scene = new THREE.Scene();
 
+// // Свет всю сцену освещает без теней
+// const ambientLight = new THREE.AmbientLight("white", 0.5);
+// scene.add(ambientLight);
+
+// // свет типо солнца есть тени у объектов
+// const dirLight = new THREE.DirectionalLight("white", 1);
+// dirLight.position.set(5, 5, 5);
+// scene.add(dirLight);
+
+// свет поинт лайт типо лампочка
+// const pointLight = new THREE.PointLight("white", 10, 100);
+// pointLight.position.set(0.5, 1, 1);
+// scene.add(pointLight);
+
+// const pointLightHelper = new THREE.PointLightHelper(pointLight, 0.5);
+// scene.add(pointLightHelper);
+
+// свет спот лайт типо фанарик направленый луч
+const spotLight = new THREE.SpotLight("white", 1);
+spotLight.position.set(1, 1, 1);
+scene.add(spotLight);
+
+//камера
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -10,49 +33,19 @@ const camera = new THREE.PerspectiveCamera(
 );
 
 camera.position.z = 5;
-
+//рендер
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
-//тектура обычная картинка
-const texture = new THREE.TextureLoader().load("image/grass.jpeg");
-const textureMaterial = new THREE.MeshBasicMaterial({ map: texture });
 
 // создание фигру разных
 const geometry = new THREE.BoxGeometry();
-
-//const material = new THREE.MeshBasicMaterial({ color: "red" });
-
-const cube = new THREE.Mesh(geometry, textureMaterial);
-cube.position.set(-4, 0, 0);
+const material = new THREE.MeshStandardMaterial({ color: "red" });
+const cube = new THREE.Mesh(geometry, material);
+cube.position.set(0, 0, 0);
 
 scene.add(cube);
-
-const spheraGeometry = new THREE.SphereGeometry(0.5, 10, 10);
-
-const spheraMaterial = new THREE.MeshPhongMaterial({
-  color: "blue",
-  emissive: "white",
-  shininess: 100,
-});
-
-const sphera = new THREE.Mesh(spheraGeometry, spheraMaterial);
-sphera.position.set(-1, 0, 0);
-
-scene.add(sphera);
-
-const torus = new THREE.Mesh(
-  new THREE.TorusGeometry(0.7, 0.2, 16, 100),
-  new THREE.MeshBasicMaterial({ color: "green" }),
-);
-torus.position.set(1, 0, 0);
-scene.add(torus);
-
-const plane = new THREE.Mesh(new THREE.PlaneGeometry(2, 2), textureMaterial);
-plane.position.set(-2, 2, 0);
-
-scene.add(plane);
 
 // функция для постоянного рендеринга анимации
 function animate() {
@@ -60,12 +53,6 @@ function animate() {
 
   cube.rotation.x += 0.1;
   cube.rotation.y += 0.1;
-
-  sphera.rotation.x += 0.1;
-  sphera.rotation.y += 0.1;
-
-  torus.rotation.x += 0.1;
-  torus.rotation.y += 0.1;
 
   renderer.render(scene, camera);
 }
